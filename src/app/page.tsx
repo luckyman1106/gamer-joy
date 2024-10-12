e5f553ebd8bc0fa47af25a88415435fde2ed6e79
devsniper1106 <andyblake1106@gmail.com>
2024-10-12 20:15:50 +0800

    home page grid complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,20 @@ import cardData from '../../public/data/cardData.json'
 
 export default function Home() {
   return (
-    <main className="flex  md:px-[84px] md:py-[58px] px-[28px] py-[24px] justify-center  ">
-      <div className="grid w-full grid-cols-3 gap-4 grid-flow-dense sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 2xl:grid-cols-7">
+    <main className=" flex md:px-[84px] md:py-[58px] px-[28px] py-[24px] justify-center  ">
+      <div className="grid grid-flow-dense w-full grid-cols-3 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 2xl:grid-cols-7">
         {cardData.map((card, index) => (
-          <div key={index} className={`col-span-${card.colNum} row-span-${card.rowNum}`}>
+          <div key={index} className={` col-span-${card.colNum} row-span-${card.rowNum}`}>
             <Card
               imageUrl={card.imageUrl}
               detailUrl={card.detailUrl}
             />
           </div>
         ))}
+
+
       </div>
+     
     </main>
   );
 }
